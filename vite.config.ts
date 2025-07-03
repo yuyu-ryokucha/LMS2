8153dc5e377aadd934fdcbad819409f21fde0ac7
YUYU <110108254+yuyu-ryokucha@users.noreply.github.com>
2025-07-03 19:04:13 +0900

    wip

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+// vite.config.ts (一時ファイル)
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./"),
+    },
+  },
+});
--- wxt.config.ts
+++ wxt.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'wxt';
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
   srcDir: 'src', // 追加
+  vite: () => ({
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"), // https://github.com/wxt-dev/examples/tree/main/examples/react-shadcn
+      },
+    },
+  }),
 });
